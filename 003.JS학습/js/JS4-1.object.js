aa1ff17ec2e92a17116e9267c6321ee90f9e3cd6
Choisj01 <choisujin96@naver.com>
2023-08-28 16:24:22 +0900

    js 배열과 객체 연습

---
+++ 003.JS학습/js/JS4-1.object.js
@@ -0,0 +1,189 @@
+// 영화 초이스 객체 ////// object.js
+
+// 자신만의 오브젝트! - 이조은
+const zoe_obj = {};
+// 오브젝트 형만 만들고 객체내용은 아래에서!
+// 1. 영화제목
+zoe_obj.title = "이터널 션샤인";
+// 2. 감독
+zoe_obj.director = "미셸 공드리";
+// 3. 배우
+zoe_obj.actor = "짐캐리, 케이트 윈슬렛";
+// 4. 장르
+zoe_obj.genre = "로맨스";
+// 5. 관람가
+zoe_obj.ratings = "12세";
+
+// 6. 예고편
+zoe_obj.trailer = function () {
+    console.log("예고편:", "07-QBnEkgXU");
+    // 플레이어함수 호출!
+    playMovie("07-QBnEkgXU");
+}; ////// trailer 함수 ////////
+
+// 자신만의 오브젝트! - 박석호
+const tom_obj = {};
+// 오브젝트 형만 만들고 객체내용은 아래에서!
+// 1. 영화제목
+tom_obj.title = '럭키';
+
+// 2. 감독
+tom_obj.director = '이계벽';
+
+// 3. 배우
+tom_obj.actor = '유해진,조윤희';
+
+// 4. 장르
+tom_obj.genre = '코미디';
+
+// 5. 관람가
+tom_obj.ratings = '15세';
+
+// 6. 예고편
+tom_obj.trailer = function(){
+    console.log('예고편:','G2zyFe4Z5w0');
+    // 플레이어함수 호출!
+    playMovie('G2zyFe4Z5w0');
+} ///////// trailer 함수 ///////
+
+           // 자신만의 오브젝트!
+           const nick_obj = {};
+           // 오브젝트 형만 만들고 객체내용은 아래에서!
+
+           // 1. 영화제목
+           nick_obj.title = '공조2:인터내셔날';
+
+           // 2. 감독
+           nick_obj.director = '이석훈';
+
+           // 3. 배우
+           nick_obj.actor = '현빈, 유해진, 윤아, 다니엘 헨리, 진선규';
+
+           // 4. 장르
+           nick_obj.genre = '액션,코미디';
+
+           // 5. 관람가
+           nick_obj.ratings = '15세';
+
+           // 6. 예고편
+           nick_obj.trailer = function(){
+               console.log('예고편:','fzUKUfHeIYA');
+               // 플레이어함수 호출!
+               playMovie('fzUKUfHeIYA');
+           } ///////////// trailer 함수 ///////////////
+           
+
+        // 자신만의 오브젝트! - 이동엽
+        const nick_obj = {};
+        // 오브젝트 형만 만들고 객체내용은 아래에서!
+
+        // 1. 영화제목
+        nick_obj.title = '공조2:인터내셔날';
+
+        // 2. 감독
+        nick_obj.director = '이석훈';
+
+        // 3. 배우
+        nick_obj.actor = '현빈, 유해진, 윤아, 다니엘 헨리, 진선규';
+
+        // 4. 장르
+        nick_obj.genre = '액션,코미디';
+
+        // 5. 관람가
+        nick_obj.ratings = '15세';
+
+        // 6. 예고편
+        nick_obj.trailer = function(){
+            console.log('예고편:','fzUKUfHeIYA');
+            // 플레이어함수 호출!
+            playMovie('fzUKUfHeIYA');
+        } ///////////// trailer 함수 ///////////////           
+
+
+         // 자신만의 오브젝트! - 신용진
+         const shin_Obj = {};
+         // 오브젝트 형만 만들고 할당은 아래에서
+         // 1. 영화제목
+         shin_Obj.title = '말아톤';
+         // 2. 감독
+         shin_Obj.director = '정윤철';
+         // 3. 배우
+         shin_Obj.actor = '조승우,김미숙';
+         // 4. 장르
+         shin_Obj.genre = '드라마';
+         // 5. 관람가
+         shin_Obj.ratings = '전체 관람가';
+         // 6. 예고편
+         shin_Obj.trailer = function(){
+             console.log('예고편','mGgYQOiUq4s');
+             // 플레이어함수 호출
+             playMovie('mGgYQOiUq4s');
+
+         }/////////////trailer함수////////////////
+
+
+         //자신만의 오브젝트! - 유혜경
+         const hg_obj = {};
+         // 오브젝트 형만 만들고 객체 내용은 아래에서 !
+
+         // 1. 영화제목
+         hg_obj.title = '엘리멘탈';
+
+         // 2. 감독
+         hg_obj.director = "피터 손";
+
+         // 3. 배우
+         hg_obj.actor = "레아 루이스";
+
+         // 4. 장르
+         hg_obj.genre = "애니메이션";
+
+         // 5. 관람가
+         hg_obj.ratings = "전체관람가";
+
+         // 6. 예고편
+         hg_obj.trailer = 
+         function(){
+             console.log('예고편:',"BOqFRHCrN-k");
+             //플레이어 함수 호출!
+             playMovie("BOqFRHCrN-k");
+             
+         }//////////////////trailer함수/////////////////////
+
+
+         // 자신만의 오브젝트 - 박서진
+         const seo_obj = {};
+         // 오브젝트 형만 만들고 객체 내용은 아래에서!!
+         // 1. 영화제목
+         seo_obj.title = '너의 결혼식';
+
+         // 2. 영화감독
+         seo_obj.director = '이석근';
+
+         // 3. 영화배우
+         seo_obj.actor = '박보영,김영광';
+
+         // 4. 영화장르
+         seo_obj.genre = '멜로';
+
+         // 5. 관람가
+         seo_obj.ratings = '12세 이상';
+
+         // 6. 예고편
+         seo_obj.trailer = function(){
+             console.log('예고편:','HuQ6YFDYY0s');
+             // 플레이어 함수 호출
+             playMovie('HuQ6YFDYY0s');
+         
+         } /////////트레일러 함수//////////  
+
+
+
+
+
+
+
+
+
+
+

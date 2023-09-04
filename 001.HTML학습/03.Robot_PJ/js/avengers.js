e481ce4887b29ed38bb4be7ae37735147568b96d
Choisj01 <choisujin96@naver.com>
2023-09-04 15:31:48 +0900

    어벤져스 소개페이지_css,js

--- 001.HTML학습/03.Robot_PJ/js/avengers.js
+++ 001.HTML학습/03.Robot_PJ/js/avengers.js
@@ -57,6 +57,29 @@ console.log(hcode);
 // 3. 대상에 html 넣어 출력하기!
 avengers.innerHTML = hcode;
 
+// 4. 로딩 후 2초 후 avengers박스에 클래스 on 넣기
+setTimeout(()=>{
+    avengers.classList.add('on');
+},2000);
+
+// 5. 타이틀 애니 위해 한글자씩 싸기
+// 대상선정: .t1
+let mytit =qs('.t1');
+let my_text = mytit.innerText;
+// 글자담기 변수
+let tit_one = '';
+// forOf문으로 한글자씩 순회하기
+for(let x of my_text){
+    console.log(x);
+    tit_one += `<span>${x}</span>`;
+}
+
+console.log(tit_one);
+
+// 다시 타이틀에 넣기
+mytit.innerHTML = tit_one;
+
+
  /************************************************* 
        [ 객체를 위한 for in 문 ]
 

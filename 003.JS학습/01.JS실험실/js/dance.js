fa5f54a4b4f086aabc7d5d8ff484c0722ff286eb
Choisj01 <choisujin96@naver.com>
2023-09-14 16:25:46 +0900

    Update dance.js

--- 003.JS학습/01.JS실험실/js/dance.js
+++ 003.JS학습/01.JS실험실/js/dance.js
@@ -36,7 +36,7 @@ for(let i=1;i<=7;i++){
 
 hcode = `<ul>${hcode}</ul>`;
 
-  console.log('코드:',hcode);
+  // console.log('코드:',hcode);
 
 // 4. 대상요소에 코드넣기
 slidePg.innerHTML = hcode;
@@ -47,14 +47,78 @@ slidePg.innerHTML = hcode;
 
 // 이벤트 대상: window
 // 이벤트 종류: scroll
-// 위치대상: .slidePg -> slidePg변수
+// 위치대상: .tpg -> 스티키박스를 싸고 있는 부모박스
+const tpg = domFn.qs('.tpg');
+
+// 움직일 대상 : .slidePg>ul
+const target = domFn.qs('.slidePg>ul');
+
+// 윈도우 이벤트 걸기
 domFn.addEvt(window,'scroll',moveSlide);
 
 function moveSlide(){
-  // 스크롤 위치값
-  let scTop = window.scrollY;
+  // 스크롤 위치값 -> 불필요!
+  // let scTop = window.scrollY;
 
-  console.log('나야나!',domFn.getBCR(slidePg),scTop);
+  // 1. 스티키 부모박스 바운딩top값
+  let bTop =  domFn.getBCR(tpg);
+  // console.log('나야나!',bTop);
 
+  // 2. 바운딩값으로  대상 left위치 변경하기
+  // 움직일대상: 스티키박스 -> .slidePg>ul
+  // 움직이기시작은 바운딩값이 0이하일때부터!!!
+
+   // (1) 윗쪽(0초과)일때 처음위치 재설정하기
+    if(bTop > 0){
+        target.style.left = '0px';
+    }
+
+    //(2) 움직이기시작은 바운딩값이 0이하일때부터!!!
+    // 한계는 -3000px
+    else if(bTop<=0 && bTop >= -3000){
+        target.style.left = bTop + 'px';
+    }
+    // (3) 마지막 한계 이후엔 한계값으로 셋팅!
+    else{
+      target.style.left = '-3000px';
+    }
 
 } //////////// moveSlide 함수 //////////////
+
+
+//////////////////////////////////////////////
+// 추가기능 : 메뉴에 오버시 움직이는 배경 구현하기
+
+// 1. 대상선정
+// 이벤트 대상: .gnb li 
+// 변경대상 : .mbg
+const gnbList = domFn.qsa('gnb li');
+const mbg = domFn.qs('.mbg');
+// console.log('대상:',gnbList,mbg);
+
+// 2. 이벤트 설정하기
+// 이벤트 종류: mouseover/ mouseout
+gnbList.forEach(ele=>{
+  domFn.addEvt(ele,'mouseover',overFn);
+  domFn.addEvt(ele,'mouseout',outFn);
+});/////////////////forEach///////////////////////
+
+// 3. 함수만들기//////////////////////////////
+function overFn(){
+  console.log('오버:',this);
+  // 1. 오버된 li의 left위치값 읽기
+  let posL = this.offsetLeft;
+  console.log('위치:',posL);
+  // 2. 메뉴배경 보이기 + 움직이기
+  mbg.style.opacity = 1;
+  mbg.style.left = posL + 'px';
+
+}////////////overFn////////////////
+function outFn(){
+  console.log('아웃:',this);
+  // 메뉴 배경 사라지기
+  mbg.style.opacity = 0;
+}////////////outFn////////////////
+
+
+

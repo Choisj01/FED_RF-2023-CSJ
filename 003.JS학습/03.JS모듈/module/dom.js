20ab64fbbb9652749f00d76a455cc6062d973ca6
Choisj01 <choisujin96@naver.com>
2023-09-19 11:19:23 +0900

    Update dom.js

--- 003.JS학습/03.JS모듈/module/dom.js
+++ 003.JS학습/03.JS모듈/module/dom.js
@@ -1,7 +1,9 @@
 // 공통함수 JS - dom.js
 
 // DOM 함수 객체 //////////////
-const domFn = {
+// const domFn = {
+// 객체를 바로 내보냄!
+export default {
     // 요소선택함수 ////////
     qs: (x) => document.querySelector(x),
     qsEl: (el, x) => el.querySelector(x),

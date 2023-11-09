c6c0ceeacd4da77b758e34268c7a8b3a0004ae5b
Choisj01 <choisujin96@naver.com>
2023-11-09 15:34:26 +0900

    Update banner.jsx

--- 004.React학습/react-app/sj-app/my-first-react-app/src/dc/banner.jsx
+++ 004.React학습/react-app/sj-app/my-first-react-app/src/dc/banner.jsx
@@ -3,29 +3,27 @@
 // 배너데이터
 import { banData } from "./data/banner";
 
-// 배너 CSS
-import '../../src/css/banner.css';
+// 배너CSS
+import "../css/banner.css";
 
 // 배너 컴포넌트 //
-export function Banner(){
-
-    // 리스트 만들기 함수
-    const makeList = (data) => {
+export function Banner() {
+  // 리스트만들기 함수
+  const makeList = (data) => {
     console.log(data);
-    return(data.map((v,i)=>
-    <li key={i}>
-        <img src={v.src} alt="ㅎㅎ"/>
-    </li>
-    ))
-}
+    return data.map((v, i) => (
+      <li key={i}>
+        <img src={v.src} alt="ㅎㅎ" />
+      </li>
+    ));
+  };
 
-    return(
-        <div className="banner">
-            {/* 이동슬라이드 */}
-            <ul className="slider">
-                {makeList(banData.main)}
-            </ul>
-        </div>
-    );
-} ////////////Banner 컴포넌트 /////////////////
+  // 코드리턴 ////////////////
+  return (
+    <div className="banner">
+      {/* 이동슬라이드 */}
+      <ul className="slider">{makeList(banData.main)}</ul>
+    </div>
+  );
+} ////////// Banner 컴포넌트 //////////
 

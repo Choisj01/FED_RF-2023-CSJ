5f007f97e15f14d688989956cb93941412128a77
Choisj01 <choisujin96@naver.com>
2023-11-09 15:44:35 +0900

    Create MainArea.jsx

---
+++ 004.React학습/react-app/sj-app/my-first-react-app/src/dc/layout/MainArea.jsx
@@ -0,0 +1,11 @@
+// DC.com MainArea 컴포넌트
+import { Banner } from "./Banner";
+
+export function MainArea(){
+    return(
+        <main className="cont">
+            <Banner category="main"/>
+        </main>
+    );
+
+} ///////// MainArea 컴포넌트 ////////////

1a4c45caecfaae97e4bf5d66db9353c35533d8f3
Choisj01 <choisujin96@naver.com>
2023-11-10 16:20:36 +0900

    Create Layout.jsx

---
+++ 004.React학습/react-app/sj-app/my-first-react-app/src/dc/layout/Layout.jsx
@@ -0,0 +1,14 @@
+// DC.com 레이아웃 컴포넌트
+
+import { TopArea } from "./TopArea";
+import { MainArea } from "./MainArea";
+import { FooterArea } from "./FooterArea";
+
+
+export function Layout(){
+    <>
+    <TopArea />
+    <MainArea />
+    <FooterArea />
+</>
+} ///////////////Layout컴포넌트/////////////////////

50f28e2c22540cf8794de3c7802a4df84482cda3
Choisj01 <choisujin96@naver.com>
2023-11-13 15:48:28 +0900

    Update index.js

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/index.js
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/index.js
@@ -1,11 +1,18 @@
 // 메인페이지 JS - index.js
 import React from "react";
 import ReactDOM, { createRoot } from "react-dom/client";
+import { TopArea } from "./layout/TopArea";
+import { MainArea } from "./layout/MainArea";
+import { FooterArea } from "./layout/FooterArea";
 
 function App() {
 
     return(
-    <h1>나야나~!!!</h1>
+      <>
+        <TopArea/>
+        <MainArea/>
+        <FooterArea/>
+      </>
     );
 } //////////App 컴포넌트 /////////////////
 

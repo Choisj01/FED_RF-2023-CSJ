0294da51d8a065d5f4e50433c1ea51935970b432
Choisj01 <choisujin96@naver.com>
2023-11-13 11:56:41 +0900

    dd

---
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/dc/layout/Layout.jsx
@@ -0,0 +1,15 @@
+// DC.com 레이아웃 컴포넌트
+
+import { TopArea } from "./TopArea";
+import { MainArea } from "./MainArea";
+import { FooterArea } from "./FooterArea";
+
+export function Layout() {
+    return (
+        <>
+            <TopArea />
+            <MainArea />
+            <FooterArea />
+        </>
+    );
+} ///////////////Layout컴포넌트/////////////////////

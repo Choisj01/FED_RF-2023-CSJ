87b6e4631c68cdd36f344ef959bdc5bfffe5b0c8
Choisj01 <choisujin96@naver.com>
2023-11-13 16:21:27 +0900

    dd

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/layout/MainArea.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/layout/MainArea.jsx
@@ -1,9 +1,19 @@
-// Pilot PJ 메인영역 공통 컴포넌트 
+// Pilot PJ 메인영역 공통 컴포넌트
 
-export function MainArea(){
-    return(
+import { MainCont } from "../pages/MainCont";
+import { MenSub } from "../pages/MenSub";
+import { StyleSub } from "../pages/StyleSub";
+import { WomenSub } from "../pages/WomenSUb";
+
+export function MainArea(props) {
+    // props.page 속성값으로 main/men/women/style
+    return (
         <>
             <h1>메인영역</h1>
+            {props.page == "main" && <MainCont />}
+            {props.page == "men" && <MenSub />}
+            {props.page == "women" && <WomenSub/>}
+            {props.page == "style" && <StyleSub />}
         </>
     );
 } //////////////////MainArea 컴폰넌트//////////////////

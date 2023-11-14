c0b44a6265e0f3619c8b576a8f0c75af9a7e29e3
Choisj01 <choisujin96@naver.com>
2023-11-14 13:00:20 +0900

    Update Banner.jsx

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/Banner.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/Banner.jsx
@@ -10,9 +10,12 @@ import { useEffect } from "react";
 import $ from "jquery";
 import "jquery-ui-dist/jquery-ui";
 
-// 슬라이드 기능구현 함수/////////////////////////////////////////////
-function slideFn() {
-    // 2. 변수설정
+
+// 배너 컴포넌트 //
+export function Banner(props) {
+
+
+    //////////// 1. 변수설정 //////////////////////////
     //(1)애니시간
     const A_TM = 600;
     //(2)애니이징
@@ -21,25 +24,26 @@ function slideFn() {
     let cSts = 0;
     // (4) 슬라이드 순번
     let sNum = 0;
-
-    ///////// 슬라이드 이동구현 함수 /////////////////
+    
+    ///////// 2. 슬라이드 이동구현 함수 /////////////////
     ///////// 이벤트 설정 및 함수호출은 리액트 파트에서 처리함!!
     // -> 그래야 다중 컴포넌트 배치시 개별화를 할 수 있다!
     const goSlide = (e) => {
+    
         // 1. 이벤트가 발생한 요소
         const tg = e.target;
-        console.log(tg);
+        // console.log(tg);
 
         // 2. 대상선정
         // (1) 슬라이드 : 클릭된 버튼으로부터 잡아줌!
         const sldBox = $(tg).siblings(".slider");
-        // (2) 슬라이드 블릿
-        const indic = $(tg).siblings(".indic li");
+        // (2) 슬라이드 블릿 : 형제요소는 .indic임!
+        const indic = $(tg).siblings(".indic").find('li');
         // console.log('블릿:',indic);
         // (3) 슬라이드 개수
         const sCnt = sldBox.find("li").length;
         // console.log("개수:", sCnt);
-
+    
         // 3. 기능구현
         // 이동버튼 클릭시
         // 0. 광클금지
@@ -47,11 +51,11 @@ function slideFn() {
         cSts = 1; //잠금
         setTimeout(() => (cSts = 0), A_TM);
         /////////////////////////////////////
-
+    
         // 1. 오른쪽버튼 여부
         let isR = $(tg).is(".rb");
         console.log("버튼클릭!", isR);
-
+    
         // 2. 버튼별분기
         // 2-1. 오른쪽버튼
         if (isR) {
@@ -78,36 +82,33 @@ function slideFn() {
                 .css({ left: "-100%" })
                 // left값을 0으로 애니메이션
                 .animate({ left: "0" }, A_TM, A_ES);
-
+    
             // 슬라이드 순번감소(0보다 작으면 끝번호)
             sNum--;
             if (sNum < 0) sNum = sCnt - 1;
         } //////else//////
-
-        // console.log('슬순번:',sNum);
-
+    
+        console.log('슬순번:',sNum);
+    
         // 블릿해당 순번 클래스 'on'넣기 (다른 li는 제거)
         indic.eq(sNum).addClass("on").siblings().removeClass("on");
     }; /////////// goSlide 함수 ////////////
-} ////////////SlideFn////////////////
 
-// 배너 컴포넌트 //
-export function Banner(props) {
     // category - 카테고리 분류명(배너 데이터선택)
 
     // 선택데이터
     const selData = banData[props.category];
 
     // 페이지 랜더링후 실행구역
-    useEffect(() => {
-        // console.log("랜더링후!");
-        // 슬라이드 기능구현 함수 호출 : 선택데이터가 1초과일떄
-        // if (selData.length > 1) slideFn();
-    }); ///////useEffect/////////
+    // useEffect(() => {
+    //     // console.log("랜더링후!");
+    //     // 슬라이드 기능구현 함수 호출 : 선택데이터가 1초과일떄
+    //     // if (selData.length > 1) slideFn();
+    // }); ///////useEffect/////////
 
-    // 리스트만들기 함수
+    // 리스트만들기 함수 /////////////////
     const makeList = (data) => {
-        console.log(data);
+        // console.log(data);
         return data.map((v, i) => (
             <li key={i}>
                 {/* 배너이미지 */}
@@ -117,13 +118,15 @@ export function Banner(props) {
                     <h3>{v.tit1}</h3>
                     <h2>{v.tit2}</h2>
                     <p>{v.cont}</p>
-                    <button>{v.btn}</button>
+                    {/* 버튼 데이터가 없으면 출력안함 */}
+                    {v.btn!=''&&<button>{v.btn}</button>}
                 </section>
             </li>
         ));
-    };
+    }; ///////////makeList함수/////////////
 
-    // 코드리턴 ////////////////
+    /////////////////////////////////////////////////////////////////////
+    /////////////////// 코드리턴 ////////////////////////////////////////
     return (
         <div className="banner">
             {/* 이동슬라이드 */}

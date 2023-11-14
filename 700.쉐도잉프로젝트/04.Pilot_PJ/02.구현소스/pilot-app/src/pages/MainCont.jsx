90b48dc6f52596f94fe75d18cac26264df8b451a
Choisj01 <choisujin96@naver.com>
2023-11-14 16:23:54 +0900

    Update MainCont.jsx

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/MainCont.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/MainCont.jsx
@@ -1,12 +1,14 @@
 // 메인 페이지 컨텐츠 컴포넌트
 
+import { Banner } from "../modules/Banner";
+
 export function MainCont(){
 
     return(
         <>
             {/* 1. 배너페이지 */}
             <section className="page"style={{background:"lightblue"}}></section>
-            
+            <Banner/>
             <section className="page"style={{background:"lightgreen"}}></section>
             <section className="page"style={{background:"lightpink"}}></section>
             <section className="page"style={{background:"lightcoral"}}></section>

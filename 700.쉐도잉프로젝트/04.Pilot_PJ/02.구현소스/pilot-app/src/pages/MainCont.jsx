00a1e66c938ab55683cc492b243676f3cbce5199
Choisj01 <choisujin96@naver.com>
2023-11-15 15:28:28 +0900

    Update MainCont.jsx

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/MainCont.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/MainCont.jsx
@@ -1,18 +1,44 @@
 // 메인 페이지 컨텐츠 컴포넌트
 
+import { useEffect } from "react";
 import { Banner } from "../modules/Banner";
 
+// 자동스크롤 JS 불러오기
+import { autoScroll } from "../func/jquery-autoScroll";
+
 export function MainCont(){
+
+    // 메인 페이지일때만 자동스크롤 기능 적용함!
+    useEffect(()=>{ // 랜더링 후 한번만 적용!
+        console.log('랜더링OK!');
+        //자동스크롤 호출
+        autoScroll();
+
+    },[]); /////// useEffect ///////////
 
     return(
         <>
             {/* 1. 배너페이지 */}
-            <section className="page"style={{background:"lightblue"}}></section>
-            <Banner/>
-            <section className="page"style={{background:"lightgreen"}}></section>
-            <section className="page"style={{background:"lightpink"}}></section>
-            <section className="page"style={{background:"lightcoral"}}></section>
-            <section className="page"style={{background:"lightgray"}}></section>
+            <section id="ban" className="page" 
+            style={{background:'lightblue'}}>
+                <Banner />
+            </section>
+            <section className="page" 
+            style={{background:'lightcoral'}}>
+                
+            </section>
+            <section className="page" 
+            style={{background:'lightgreen'}}>
+                
+            </section>
+            <section className="page" 
+            style={{background:'lightseagreen'}}>
+                
+            </section>
+            <section className="page" 
+            style={{background:'lightpink'}}>
+                
+            </section>
         </>
     )
 

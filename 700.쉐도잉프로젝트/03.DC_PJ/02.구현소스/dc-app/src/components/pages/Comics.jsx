640000c3ea3885a7eb677c679a23a3a91d2c8d0f
Choisj01 <choisujin96@naver.com>
2023-11-16 12:25:22 +0900

    Update Comics.jsx

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Comics.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Comics.jsx
@@ -1,7 +1,7 @@
 // 코믹스페이지 메인 컨텐츠
 
 import { Banner } from "../modules/Banner";
-import { VidIntro } from "../modules/vidintro";
+import { VidIntro } from "../modules/VidIntro";
 
 export function Comics() {
     return (

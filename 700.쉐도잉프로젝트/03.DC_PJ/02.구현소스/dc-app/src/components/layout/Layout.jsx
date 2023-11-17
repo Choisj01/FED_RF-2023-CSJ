7209098094df1316120ae4e25cd1d6f52eb591f6
Choisj01 <choisujin96@naver.com>
2023-11-17 13:15:22 +0900

    Update Layout.jsx

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/layout/Layout.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/layout/Layout.jsx
@@ -1,15 +1,43 @@
-// DC.com 레이아웃 컴포넌트
+// DC.com 레이아웃 컴포넌트 : 실제적인 최상위 컴포넌트임!
 
-import { TopArea } from "./TopArea";
-import { MainArea } from "./MainArea";
 import { FooterArea } from "./FooterArea";
+import { MainArea } from "./MainArea";
+import { TopArea } from "./TopArea";
 
-export function Layout() {
-    return (
-        <>
-            <TopArea />
-            <MainArea />
-            <FooterArea />
-        </>
+// Context API 불러오기
+import { dcCon } from "../modules/dcContext";
+import { useNavigate } from "react-router-dom";
+// import { useEffect } from "react";
+import { useLayoutEffect } from "react";
+
+export function Layout(){
+
+  // 랜더링 후(화면보이기 전) 실행구역 //////////////////////////
+  useLayoutEffect(()=>{
+    // 페이지 이동시 스크롤위치 상단이동
+    window.scrollTo(0,0);
+  }); ///////// useEffect //////////////
+
+  // 라우터 이동객체설정
+  const goNav = useNavigate();
+
+  // 라우터 이동함수
+  const chgPage = (txt) => goNav(txt); 
+
+
+  /************************************************************************ 
+    [ 컨텍스트 API 공유값 설정 ]
+    1. chgPage 함수 : 라우터 이동 기능 
+  ************************************************************************/
+
+  // 리턴코드//////////////////////////
+    return(
+    <dcCon.Provider value={{chgPage}}>
+      <TopArea />
+      <MainArea />
+      <FooterArea />
+    </dcCon.Provider>
+
     );
-} ///////////////Layout컴포넌트/////////////////////
+
+} /////////// Layout 컴포넌트 ///////////

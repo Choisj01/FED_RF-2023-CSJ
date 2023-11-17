c1bcddedf22ef0137073952fcb3c8f46bcd08780
Choisj01 <choisujin96@naver.com>
2023-11-17 11:12:56 +0900

    Update SwiperApp.jsx

--- 004.React학습/react-app/sj-app/my-first-react-app/src/dc/plugin/SwiperApp.jsx
+++ 004.React학습/react-app/sj-app/my-first-react-app/src/dc/plugin/SwiperApp.jsx
@@ -29,9 +29,35 @@ export function SwiperApp() {
     "dcm12",
   ];
 
+  // 상태관리변수 : 멈춤상태 / 플레이상태
+  const [sts,setSts]=useState(1);
+
+
+  // useRef로 스와이퍼 객체를 받아온다!
+  const myRef = useRef(null);
+  // Swiper 컴포넌트의 ref속성에 담아서 연결!!!
+
+  // 플레이/멈춤 기능 함수
+  const stopPlay = () => {
+    console.log('멈추거나 플레이!');
+    // sts값이 1이면 멈춤!
+    sts?
+    myRef.current.swiper.autoplay.stop():
+    myRef.current.swiper.autoplay.start();
+
+    // 상태값 업데이트
+    sts? setSts(0):setSts(1);
+
+
+
+  }; ///////////stopPlay함수////////////
+
+  // 리턴코드 ////////////////////////////////////////
   return (
     <>
       <Swiper
+      ///* ref 속성에 useRef할당변수를 넣어서 외부에 연결함 */
+      ref={myRef}
         slidesPerView={3}
         spaceBetween={30}
         pagination={{
@@ -55,6 +81,18 @@ export function SwiperApp() {
         }        
        
       </Swiper>
+      {/* 플레이멈춤 버튼 */}
+      <button className="stopPlay"
+      style={{
+        backgroundColor:'transparent',
+        border:'none',
+        fontSize:'40px',
+        display:'block',
+        margin: '0 auto',
+        cursor:'pointer',
+      }} title={sts?'멈추기':'자동넘기기'}
+      onClick={stopPlay}
+      >{sts?'▣':'▶'}</button>
     </>
   );
 } /////////// SwiperApp 컴포넌트 ///////////

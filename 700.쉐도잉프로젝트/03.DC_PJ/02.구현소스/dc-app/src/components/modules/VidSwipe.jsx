8e964d0e73f5106d4feec0c239678a4e94fd0bb8
Choisj01 <choisujin96@naver.com>
2023-11-20 12:29:37 +0900

    dcPJ 메인 하위 비디오 스와이프 모듈

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/VidSwipe.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/VidSwipe.jsx
@@ -14,6 +14,7 @@ section.vidswbox >
    
 // 비디오 스와이프 CSS 불러오기 
 import '../../css/vid_swipe.css'
+import { SwiperVid } from '../plugin/SwiperVId';
 
 export function VidSwipe(){
 
@@ -22,21 +23,22 @@ export function VidSwipe(){
     return(
         <>
         {/* 모듈코드 */}
-         <section className="vidswbox">
+         <section className="vid-swbox">
             {/* 1. 모듈 타이틀 */}
-            <h2 className="tit"></h2>
-            {/* 2. 스와이퍼 컴포넌트 */}
+            <h2 className="tit">LATEST TRAILERS, CLIPS & MORE</h2>
+            {/* 2. 스와이퍼 컴포넌트 : SwiperVid */}
+            <SwiperVid/>
 
             {/* 3. 비디오 재생창 */}
-            <section className="vidbx">
+            <section className="vid-bx">
                 {/* 비디오 중앙 박스 */}
-                <div className="playvid">
+                <div className="play-vid">
                     {/* 비디오 타이틀 */}
-                    <h2 className="ifrtit"></h2>
+                    <h2 className="ifr-tit"></h2>
                     {/* 아이프레임 */}
                     <iframe src="" allow="autoplay"></iframe>
                     {/* 닫기버튼 */}
-                    <button className="cbtn"></button>
+                    <button className="cbtn">×</button>
                 </div>
             </section>
          </section>

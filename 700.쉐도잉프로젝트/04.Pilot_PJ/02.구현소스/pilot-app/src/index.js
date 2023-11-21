27dcf21b3388aa2094cc1e521b17a3f79ef490e9
Choisj01 <choisujin96@naver.com>
2023-11-21 11:54:53 +0900

    Update index.js

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/index.js
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/index.js
@@ -26,7 +26,27 @@ function App(){
 
   // 랜더링 후 실행구역 ////////////
   useEffect(()=>{
+    // 햄버거버튼 클릭시 전체메뉴 보이기/숨기기
+    $('.ham').click(e=>{
+      //전체메뉴박스: .mbox -> 보이기/숨기기
+      $('.mbox').fadeToggle(400);
+      // 햄버거버튼에 클래스 'on' 넣기 /빼기
+      $(e.currentTarget).toggleClass('on');
+      // e.target과 e.currentTarget은 다르다!
+      // 후자가 햄버거버튼 자신임! e.target은 span하나하나로 찍힘.
+      //console.log(e.currentTarget);
 
+      // 3. 비디오 재생 / 멈춤 : 대상 - .bgm
+      //get(0) 은 비디오 컬렉션이고 그중 0번째를 의미함 
+      // -> 제이쿼리용!(일반JS에서는 선택자만 입력하면 됨)
+      const vid = $('.bgm').get(0)
+      vid.paused? vid.play():vid.pause();
+      console.log(vid.paused);
+      // paused 속성 : 동영상 멈춤일때 true 리턴
+      // play() 메서드 : 동영상 재상 메서드
+      // pause() 메서드 : 동영상 정지 메서드
+    }); ////////////click ////////////
+  
 
   }); ////////// useEffect //////////////
 

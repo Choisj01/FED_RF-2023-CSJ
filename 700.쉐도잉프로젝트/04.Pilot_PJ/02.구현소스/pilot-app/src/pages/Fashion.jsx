bfdcbcaba1ec4e95da49f2f051b6911a35f8329c
Choisj01 <choisujin96@naver.com>
2023-11-22 15:43:24 +0900

    Update Fashion.jsx

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/Fashion.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/Fashion.jsx
@@ -1,12 +1,35 @@
 // 공통패션 서브페이지 컨텐츠 컴포넌트
 
-export function Fashion(props){
+import { useEffect } from "react";
+
+// 공토 서브 CSS 불러오기
+import '../css/fashion.css';
+import { SwiperApp } from "../plugin/SwiperApp";
+
+export function Fashion(props) {
     // props.cat - 서브 카테고리명
 
-    return(
-        <>
-            <h2>{props.cat}</h2>
-        </>
-    )
+    useEffect(() => {
+        document.querySelector("html").style.overflow = "visible"; /* hidden의 반대 */
+        document.querySelector("body").style.overflow = "visible"; /* hidden의 반대 */
+    }, []);
 
+    return (
+        <>
+            {/* 1. 배너영역 */}
+            <section id="ban" className="page">
+                <SwiperApp/>
+            </section>
+            {/* 2. 신상픔 영역 */}
+            <section id="c1" className="cont c1 men"></section>
+            {/* 2.5. 상세보기 박스 */}
+            <div className="bgbx"></div>
+            {/* 3. 패럴렉스 영역 */}
+            <section id="c2" className="cont c2"></section>
+            {/* 4. 단일상품 영역 */}
+            <section id="c3" className="cont c3"></section>
+            {/* 5. 스타일상품 영역 */}
+            <section id="c4" className="cont c4"></section>
+        </>
+    );
 } /////////////// Fashion 컴포넌트 //////////////////

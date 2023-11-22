158bb356738cce4586410e102319998e1391a79c
Choisj01 <choisujin96@naver.com>
2023-11-22 13:17:44 +0900

    Update TotalMenu.jsx

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/TotalMenu.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/TotalMenu.jsx
@@ -1,17 +1,32 @@
 //Pilot PJ 전체메뉴 컴포넌트
 
+// 컨텍스트 API 불러오기
+import { useContext } from "react";
+import { pCon } from "./PilotContext";
+
 export function TotalMenu() {
+    // 컨텍스트 사용
+    const myCon = useContext(pCon);
+    // pCon에 Provider value 속성에 공개한 변수/함수를 사용함!
+
+    // 메뉴이동 처리함수 //////////
+    const goPage = (txt) => {
+        myCon.chgPgName(txt);
+        // 전체박스 숨기기
+        document.querySelector(".ham").click();
+    }; //////////// goPage 메서드 /////////
+
+    // 코드리턴 /////////////////////////////////
     return (
         <>
             <div className="mbox">
-                <video 
-                src="./images/disc2018.mp4" 
-                loop="loop" muted="muted" 
-                className="bgm"></video>
+                <video src="./images/disc2018.mp4" loop="loop" muted="muted" className="bgm"></video>
                 <nav className="mlist">
                     <dl>
                         <dt>
-                            <a href="#">MEN</a>
+                            <a href="#" onClick={() => goPage("men")}>
+                                MEN
+                            </a>
                         </dt>
                         <dd>
                             <a href="#">T-SHIRT</a>
@@ -28,7 +43,9 @@ export function TotalMenu() {
                     </dl>
                     <dl>
                         <dt>
-                            <a href="#">WOMEN</a>
+                            <a href="#" onClick={() => goPage("women")}>
+                                WOMEN
+                            </a>
                         </dt>
                         <dd>
                             <a href="#">T-SHIRT</a>
@@ -45,7 +62,9 @@ export function TotalMenu() {
                     </dl>
                     <dl>
                         <dt>
-                            <a href="#">STYLE</a>
+                            <a href="#" onClick={() => goPage("style")}>
+                                STYLE
+                            </a>
                         </dt>
                         <dd>
                             <a href="#">COLLECTION</a>

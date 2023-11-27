b6771a76e0e7d24bcbd62372dbfd006449d281ac
Choisj01 <choisujin96@naver.com>
2023-11-27 12:52:39 +0900

    Update CatList.jsx

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/CatList.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/CatList.jsx
@@ -1,32 +1,36 @@
 // DC PJ 캐릭터 리스트 컴포넌트
 
-// 캐릭터리스트 모듈 CSS 불러오기
-import '../../css/cat_list.css';
-import { SwiperCat } from '../plugin/SwiperCat';
+// 캐릭트 리스트 모듈 CSS 불러오기
+import "../../css/cat_list.css"
+import { SwiperCat } from "../plugin/SwiperCat"
 
 /* 
-     Root > 
-        section.cat-swbox > 
-            h2.tit + 스와이퍼컴포넌트(<SwiperCat />)
-            _________________________________________________
-            -> 스와이퍼 컴포넌트 구조 :
-            스와이퍼 리스트 > 
-                a링크(Link />) >
-                    section.sw-inbox2 > img
-                    + 
-                    div.cat-tit2 > h3
+Root >
+    section.cat-swbox >
+        h2.tit + 스와이퍼 컴포넌트(<SwiperCat />)
+    _______________________________
+
+    -> 스와이퍼 컴포넌트 구조:
+        스와이퍼 리스트 >
+            a링크(<Link>) >
+                section.sw-inbox2 > img
+                +
+                div.cat-tit2 > h3
 */
+
 export function CatList(props){
 
     return(
         <>
-            <section className="cat-swbox">
-                {/* 1. 모듈 타이틀 */}
-                <h2 className="tit">WHO'S WHO: THE JUSTICE LEAGUE</h2>
+           <section className="cat-swbox">
+                {/* 1. 모듈타이틀 */}
+                <h2 className="tit">
+                WHO'S WHO: THE JUSTICE LEAGUE
+                </h2>
                 {/* 2. 스와이퍼 컴포넌트 */}
                 <SwiperCat />
-            </section>
+           </section>
         </>
-    );
+    )
 
-} //////////CatList 컴포넌트//////////////
+} /////////// CatList 컴포넌트 /////////////

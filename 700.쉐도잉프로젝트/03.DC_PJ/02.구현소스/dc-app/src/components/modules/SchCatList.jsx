34509d86c690bd7f73580b0aa8c3eca2130ed974
Choisj01 <choisujin96@naver.com>
2023-11-27 12:52:43 +0900

    Update SchCatList.jsx

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/SchCatList.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/SchCatList.jsx
@@ -7,36 +7,62 @@ import { catListData } from "../data/swiper_cat";
 
 // 캐릭터 검색 리스트 CSS
 import '../../css/search_cat_list.css';
+import { text } from "@fortawesome/fontawesome-svg-core";
 
-export function SchCatList(props) {
+export function SchCatList({word,chgCntFn}) {
     // props.word - 데이터 선택값
+    // props.showCntFn - 개수 보이기 함수 전달
+
+
+    // 전달된 검색어 소문자 변환
+    let kword = word.toLowerCase();
 
     // 선택데이터
     const selData = catListData.filter(v=>{
-        if(v.cname.toLowerCase().indexOf(props.word) !== -1) return true;
+        if(v.cname.toLowerCase().indexOf(kword) !== -1) return true;
     });
 
-    console.log(selData);
+    // 선택데이터 개수
+    const selCnt = selData.length;
+
+    // 선택데이터 개수 후크변수 업데이트하기
+    chgCntFn(selCnt);
+
+    console.log(selData,selCnt);
 
     return (
         <>
-            <ul className="clist">
+          {
+            // 데이터 개수가 0이 아닐때
+            selCnt != 0 && (
+              <ul className="clist">
                 {selData.map((v, i) => (
-                    <li key={i}>
-                        <Link
-                            to="/detail"
-                            state={{
-                                cname: v.cname,
-                                cdesc: v.cdesc,
-                                facts: v.facts,
-                            }}
-                        >
-                            <img src={v.tmsrc} alt={v.cname} />
-                            <h3>{v.cname}</h3>
-                        </Link>
-                    </li>
+                  <li key={i}>
+                    <Link
+                      to="/detail"
+                      state={{
+                        cname: v.cname,
+                        cdesc: v.cdesc,
+                        facts: v.facts,
+                      }}
+                    >
+                      <img src={v.tmsrc} alt={v.cname} />
+                      <h3>{v.cname}</h3>
+                    </Link>
+                  </li>
                 ))}
-            </ul>
+              </ul>
+            )
+          }
+          {
+            // 선택데이터가 0개이면
+            selCnt == 0 && (
+              <h2 style={{textAlign:'center'}}>
+                Sorry, we don't have any matches for that. But there's plenty more
+                to see on DC!
+              </h2>
+            )
+          }
         </>
-    );
-} ////////////////// SchCatList 컴포넌트 //////////////////////
+      );
+    } ///////// SchCatList /////////////////

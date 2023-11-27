688d3fe9c7c2f0e1898ec7d6179f0b4a5ceb1ef3
Choisj01 <choisujin96@naver.com>
2023-11-27 14:33:43 +0900

    Update SinSang.jsx

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/SinSang.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/SinSang.jsx
@@ -1,33 +1,35 @@
-// 신상품 컴포넌트 /////////////
+// 신상품 컴포넌트 ////////
 
 export function SinSang(props) {
     // props.cat - 카테고리 분류명
-
+  
     const makeList = () => {
-        let temp = [];
-        for(let x=0;x<=9;x++){
-            temp[x] = (<li className={"m"+(x+1)} key={x}>
+      let temp = [];
+      for (let x = 0; x < 9; x++) {
+        temp[x] = (
+          <li className={"m" + (x + 1)} key={x}>
             <a href="#">
-                <img src={"./images/goods/"+props.cat+"/m"+(x+1)+".png"} alt="신상품" />
+              <img
+                src={"./images/goods/" + props.cat + "/m" + (x + 1) + ".png"}
+                alt="신상품"
+              />
             </a>
-        </li>
+          </li>
         );
-        } ////////// for ////////////////
-        return temp;
-    }  //////////////// makeList 함수 ///////////////////
-
-    // 리턴코드 //////////
+      } ///// for /////
+      return temp;
+    }; ///////// makeList 함수 ///////////
+  
+    // 리턴 코드 /////////////////////
     return (
-        <>
-            <h2 className="c1tit">
-                NEW MEN'S ARRIVAL
-                <button>전체리스트</button>
-            </h2>
-            <div className="flowbx">
-                <ul className="flist">
-                    {makeList()}
-                </ul>
-            </div>
-        </>
+      <>
+        <h2 className="c1tit">
+          NEW MEN'S ARRIVAL
+          <button>전체리스트</button>
+        </h2>
+        <div className="flowbx">
+          <ul className="flist">{makeList()}</ul>
+        </div>
+      </>
     );
-} //////////////// SinSang 컴포너트 //////////////////
+  } ////////////// SinSang 컴포넌트 //////////

a2dfafe1b1291e59b3696e67d5339ddfb1ac7d78
Choisj01 <choisujin96@naver.com>
2023-11-28 11:20:24 +0900

    Update SinSang.jsx

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/SinSang.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/SinSang.jsx
@@ -1,10 +1,16 @@
 // 신상품 컴포넌트 ////////
 
+import { useEffect } from "react";
+
+import $ from 'jquery';
+
 export function SinSang(props) {
     // props.cat - 카테고리 분류명
   
     const makeList = () => {
+      // 코드 담을 배열
       let temp = [];
+      // 원하는 반복수만큼 for문 실행하여 배열에 JSX태그 담기
       for (let x = 0; x < 9; x++) {
         temp[x] = (
           <li className={"m" + (x + 1)} key={x}>
@@ -17,8 +23,54 @@ export function SinSang(props) {
           </li>
         );
       } ///// for /////
+      // JSX 태그를 담은 배열을 리턴 -> 자동 태그변환!
       return temp;
     }; ///////// makeList 함수 ///////////
+
+    // 신상품 리스트 이동함수 사용변수 /////////
+    // 위치값 변수(left값)
+    let lpos = 0;
+    // 재귀호출 상태값( 1 - 호출 , 0 - 멈춤)
+    let callSts = 1;
+
+    // 신상품 리스트 이동함수
+    const flowList = (ele) => { // ele - 움직일 대상
+
+      // console.log(ele);
+      // 대상의 left값을 1씩 감소함
+      lpos--;
+      
+      // 이미지 박스 1개가 나가면 잘라서 맨뒤로 보냄(한쪽방향으로만 이동)
+      if(lpos<-300){
+        // 위치값 초기화(-300일떄 0으로 변경!)
+        lpos=0;
+        //첫번쨰 li 맨뒤로 이동
+        ele.append(ele.find('li').first());
+      } //////////  if ////////////
+      
+       // 적용함
+       ele.css({left:lpos+'px'})
+
+      // 재귀호출
+      if(callSts)
+      setTimeout(() => flowList(ele),40)
+
+    }; //////// flowList 함수 ///////////
+
+    //마우스 오버/아웃시 이동제어 함수
+    const flowOut = () => {
+
+    }; ////////flowOut 함수/////////////
+
+    // 랜더링 후 실행구역 ////////////
+    useEffect(()=>{
+      // 대상선정: .flist
+
+
+      // 신상일스트 이동함수 호출!
+      flowList($('.flist'))
+
+    }); //////// useEffect ////////////
   
     // 리턴 코드 /////////////////////
     return (
@@ -27,7 +79,9 @@ export function SinSang(props) {
           NEW MEN'S ARRIVAL
           <button>전체리스트</button>
         </h2>
-        <div className="flowbx">
+        <div className="flowbx" 
+        onMouseOver={()=>callSts=0} 
+        onMouseOut={()=>{callSts=1;flowList($('.flist'));}}>
           <ul className="flist">{makeList()}</ul>
         </div>
       </>

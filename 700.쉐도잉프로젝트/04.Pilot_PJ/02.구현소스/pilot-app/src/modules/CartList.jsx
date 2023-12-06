eaa7a090e23b831308d6b230dcba2fd45cfa5b33
Choisj01 <choisujin96@naver.com>
2023-12-06 11:54:42 +0900

    Update CartList.jsx

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/CartList.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/CartList.jsx
@@ -1,13 +1,20 @@
 // Pilot PJ 장바구니 리스트 컴포넌트
 
 // 장바구니 리스트 CSS 불러오기
-import '../css/cartlist.css';
+import "../css/cartlist.css";
 
-export function CartList(){
+export function CartList() {
+    // 선택 데이터 : 로컬스토리지 데이터를 객체변환!
+    const selData = JSON.parse(localStorage.getItem("cart"));
 
-    console.log(localStorage.getItem('cart'));
+    console.log(localStorage.getItem("cart"));
 
-    return(
+    //정규식함수(숫자 세자리마다 콤마해주는 기능)
+    function addComma(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
+    return (
         <>
             <section id="cartlist">
                 <a href="#" className="cbtn cbtn2">
@@ -15,96 +22,54 @@ export function CartList(){
                 </a>
                 <table>
                     <caption>
-                        <h1> 카트 리스트</h1> 
+                        <h1> 카트 리스트</h1>
                     </caption>
-                    <tbody><tr>
-                        <th>상품</th>
-                        <th>번호</th>
-                        <th>상품명</th>
-                        <th>상품코드</th>
-                        <th>단가</th>
-                        <th>수량</th>
-                        <th>합계</th>
-                        <th>삭제</th>
-                    </tr>
-                    
-                    <tr>
-                        <td>
-                            <img src="images/goods/women/m8.png" alt="item"/>
-                        </td>
-                        <td>1</td>
-                        <td>[여성]베이직 솔리드 래쉬가드</td>
-                        <td>DMSW15731-BK </td>
-                        <td>49,000원</td>
-                        <td>3</td>
-                        <td>147,000원</td>
-                        <td>
-                            <button className="cfn" data-idx="20">
-                                ×
-                            </button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <img src="images/goods/women/m8.png" alt="item"/>
-                        </td>
-                        <td>1</td>
-                        <td>[여성]베이직 솔리드 래쉬가드</td>
-                        <td>DMSW15731-BK </td>
-                        <td>49,000원</td>
-                        <td>3</td>
-                        <td>147,000원</td>
-                        <td>
-                            <button className="cfn" data-idx="20">
-                                ×
-                            </button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <img src="images/goods/women/m8.png" alt="item"/>
-                        </td>
-                        <td>1</td>
-                        <td>[여성]베이직 솔리드 래쉬가드</td>
-                        <td>DMSW15731-BK </td>
-                        <td>49,000원</td>
-                        <td>3</td>
-                        <td>147,000원</td>
-                        <td>
-                            <button className="cfn" data-idx="20">
-                                ×
-                            </button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <img src="images/goods/women/m8.png" alt="item"/>
-                        </td>
-                        <td>1</td>
-                        <td>[여성]베이직 솔리드 래쉬가드</td>
-                        <td>DMSW15731-BK </td>
-                        <td>49,000원</td>
-                        <td>3</td>
-                        <td>147,000원</td>
-                        <td>
-                            <button className="cfn" data-idx="20">
-                                ×
-                            </button>
-                        </td>
-                    </tr>
+                    <tbody>
+                        <tr>
+                            <th>상품</th>
+                            <th>번호</th>
+                            <th>상품명</th>
+                            <th>상품코드</th>
+                            <th>단가</th>
+                            <th>수량</th>
+                            <th>합계</th>
+                            <th>삭제</th>
+                        </tr>
+                    {selData.map((v,i)=>(
+                        <tr key={i}>
+                            {/* 상품이미지 */}
+                            <td>
+                                <img src={"images/goods/"+v.cat+"/"+v.ginfo[0]+".png"} 
+                                alt="item" />
+                            </td>
+                            {/* 리스트 순번 */}
+                            <td>1</td>
+                            {/* 상품명 */}
+                            <td>{v.ginfo[1]}</td>
+                            {/* 상품코드 */}
+                            <td>{v.ginfo[2]}</td>
+                            {/* 상품가격 */}
+                            <td>{addComma(v.ginfo[3])}원</td>
+                            {/* 상품수량 */}
+                            <td>{v.num}</td>
+                            {/* 상품가격 총합계 */}
+                            <td>{addComma(v.ginfo[3]*v.num)}원</td>
+                            <td>
+                                <button className="cfn" data-idx={v.idx}>
+                                    ×
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
 
-                    <tr>
-                        <td colSpan="6">
-                            총합계 : 
-                        </td>
-                        <td>
-                            147,000원
-                        </td>
-                        <td></td>
-                    </tr>
-                </tbody></table>
+                        <tr>
+                            <td colSpan="6">총합계 :</td>
+                            <td>147,000원</td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
             </section>
         </>
     );
-
 } /////////////////CartList 컴포넌트 ////////////////////////

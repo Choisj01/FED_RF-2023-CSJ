85ee412ebd6ce99c7e7aaf025e44698f7ebfdc9a
Choisj01 <choisujin96@naver.com>
2023-12-12 12:20:59 +0900

    Update Layout.jsx

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/layout/Layout.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/layout/Layout.jsx
@@ -7,42 +7,59 @@ import { TopArea } from "./TopArea";
 // Context API 불러오기
 import { dcCon } from "../modules/dcContext";
 import { useNavigate } from "react-router-dom";
-import { useCallback, useLayoutEffect } from "react";
+import { useCallback, useLayoutEffect, useState } from "react";
 
-export function Layout(){
+export function Layout() {
+    //************ Hook 상태관리 변수 ****************** */
+    // 1. 로그인 상태 체크 변수 : 로컬스 'minfo' 초기할당!
+    const [logSts, setLogSts] = useState(localStorage.getItem("minfo"));
 
-  // 랜더링 후(화면보이기 전) 실행구역 //////////////////////////
-  useLayoutEffect(()=>{
-    // 페이지 이동시 스크롤위치 상단이동
-    window.scrollTo(0,0);
-  }); ///////// useEffect //////////////
+    // 2. 로그인 환영메시지 상태변수
+    const [logMsg, setLogMsg] = useState(null);
 
-  // 라우터 이동객체설정
-  const goNav = useNavigate();
+    ////////////////////////////////////
+    // 로그아웃 함수 ///////////////////
+    // -> TopArea 컴포넌트에 전달함!
+    const logOut = useCallback(()=>{
+      // 1. 로컬스 삭제(minfo)
+      localStorage.removeItem('minfo');
+      // 2. 로그인 상태값 업데이트
+      setLogSts(null);
+      // 3. 로그인 메시지 업데이트
+      setLogMsg(null);
+    },[]); ////////// logOut 함수 //////////
 
-  // 라우터 이동함수 : pgName - 페이지 이름 / param - 전달값
-  const chgPage = useCallback((pgName,param) => goNav(pgName,param),[]); 
-  // 메모이제이션되는 TopArea 컴포넌트에 제공하는 함수가 
-  // useCallback을 사용한 메모이제이션 처리되어야 변경없는 것을
-  // 체크하여 함수를 업데이트하지 않는다!
-  // useCallback(기존익명함수,[의존성])
-  // -> 의존성 변수가 없을때 비워놓아도 효과있음!(단, 형식을 맞출 것!)
-  
+    // 랜더링 후(화면보이기 전) 실행구역 //////////////////////////
+    useLayoutEffect(() => {
+        // 페이지 이동시 스크롤위치 상단이동
+        window.scrollTo(0, 0);
+    }); ///////// useEffect //////////////
 
-  /************************************************************************ 
+    // 라우터 이동객체설정
+    const goNav = useNavigate();
+
+    // 라우터 이동함수 : pgName - 페이지 이름 / param - 전달값
+    const chgPage = useCallback((pgName, param) => goNav(pgName, param), []);
+    // 메모이제이션되는 TopArea 컴포넌트에 제공하는 함수가
+    // useCallback을 사용한 메모이제이션 처리되어야 변경없는 것을
+    // 체크하여 함수를 업데이트하지 않는다!
+    // useCallback(기존익명함수,[의존성])
+    // -> 의존성 변수가 없을때 비워놓아도 효과있음!(단, 형식을 맞출 것!)
+
+    /************************************************
     [ 컨텍스트 API 공유값 설정 ]
     1. chgPage 함수 : 라우터 이동 기능 
-  ************************************************************************/
+    2. setLogSts : 로그인 상태값 업데이트
+    3. setLogMsg : 로그인 메시지 업데이트
+  ****************************************************/
 
-  // 리턴코드//////////////////////////
-    return(
-    <dcCon.Provider value={{chgPage}}>
-      {/* 메모이제이션 관리를 위해 함수를 컨텍스트방식이 아닌 속성으로 직접보냄! */}
-      <TopArea chgPageFn={chgPage} />
-      <MainArea />
-      <FooterArea />
-    </dcCon.Provider>
-
+    // 리턴코드//////////////////////////
+    return (
+        <dcCon.Provider value={{ chgPage, setLogSts, setLogMsg }}>
+            {/* 메모이제이션 관리를 위해 함수를 컨텍스트방식이 아닌 속성으로 직접보냄! */}
+            <TopArea chgPageFn={chgPage} logSts={logSts} logMsg={logMsg} logOut={logOut} />
+            <MainArea />
+            <FooterArea />
+        </dcCon.Provider>
     );
-
 } /////////// Layout 컴포넌트 ///////////

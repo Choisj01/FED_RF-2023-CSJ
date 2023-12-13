13e1a9af2548940ba14901935d93816dc50e8e36
Choisj01 <choisujin96@naver.com>
2023-12-13 13:18:25 +0900

    Update CartList.jsx

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/CartList.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/CartList.jsx
@@ -8,116 +8,134 @@ import "../css/cartlist.css";
 import $ from "jquery";
 
 // 전달값이 변경되면 리랜더링하기 위해 메모이제이션을 적용!
-export const CartList = memo(({selData}) => {
-  // 로컬 스토리지 데이터를 props로 전달 받는다!
+export const CartList = memo(({ selData }) => {
+    // 로컬 스토리지 데이터를 props로 전달 받는다!
 
-  // 선택 데이터 : 로컬스토리지 데이터를 객체변환! -> 주석처리
-  // const selData = JSON.parse(localStorage.getItem("cart"));
+    // 선택 데이터 : 로컬스토리지 데이터를 객체변환! -> 주석처리
+    // const selData = JSON.parse(localStorage.getItem("cart"));
 
-  // 데이터개수
-  const cntData = selData.length;
+    // 데이터개수
+    const cntData = selData.length;
 
-  console.log(selData,cntData+'개');
+    console.log(selData, cntData + "개");
 
-  //정규식함수(숫자 세자리마다 콤마해주는 기능)
-  function addComma(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  }
+    // 전체합계 구하기
+    let totalCnt = 0;
+    selData.forEach((v) => {
+        totalCnt += v.ginfo[3] * v.num;
+    }); //////// forEach ///////////
 
-  // 랜더링후 실행구역 /////////////
-  useEffect(() => {
-    // 카트버튼 나타나기
-    $("#mycart")
-    .removeClass('on')
-    .fadeIn(300, function () {
-      // 페이드 애니후
-      $(this).addClass('on');
-    }) ////// fadeIn ////////
+    console.log("토달:", totalCnt);
 
-    console.log("나야나");
-  }, []); /// useEffect ///////////////
+    //정규식함수(숫자 세자리마다 콤마해주는 기능)
+    function addComma(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
 
-  // 리스트 보이기 함수 //////////
-  const showList = () => {
-    console.log('열려라!!');
-    $("#cartlist").animate({right:'0'},600);
-  }; //////////// showList ////////////
+    // 랜더링후 실행구역 /////////////
+    useEffect(() => {
+        // 카트버튼 나타나기
+        $("#mycart")
+            .removeClass("on")
+            .fadeIn(300, function () {
+                // 페이드 애니후
+                $(this).addClass("on");
+            }); ////// fadeIn ////////
 
-  // 리스트 숨기기 함수 //////////
-  const hideList = (e) => {
-    e.preventDefault();
-    $("#cartlist").animate({right:'-60%'},600);
-  }; //////////// hideList ////////////
+        console.log("나야나");
+    }, []); /// useEffect ///////////////
 
-  /// 리턴 코드 ///////////////////////
-  return (
-    <>
-      <section id="cartlist">
-        <a href="#" className="cbtn cbtn2"
-        onClick={hideList}>
-          <span>닫기버튼</span>
-        </a>
-        <table>
-          <caption>
-            <h1> 카트 리스트</h1>
-          </caption>
-          <tbody>
-            <tr>
-              <th>상품</th>
-              <th>번호</th>
-              <th>상품명</th>
-              <th>상품코드</th>
-              <th>단가</th>
-              <th>수량</th>
-              <th>합계</th>
-              <th>삭제</th>
-            </tr>
+    // 리스트 보이기 함수 //////////
+    const showList = () => {
+        console.log("열려라!!");
+        $("#cartlist").animate({ right: "0" }, 600);
+    }; //////////// showList ////////////
 
-            {selData.map((v, i) => (
-              <tr key={i}>
-                {/* 상품이미지 */}
-                <td>
-                  <img
-                    src={"images/goods/" + v.cat + "/" + v.ginfo[0] + ".png"}
-                    alt="item"
-                  />
-                </td>
-                {/* 리스트순번 */}
-                <td>{i + 1}</td>
-                {/* 상품명 */}
-                <td>{v.ginfo[1]}</td>
-                {/* 상품코드 */}
-                <td>{v.ginfo[2]}</td>
-                {/* 상품가격 */}
-                <td>{addComma(v.ginfo[3])}원</td>
-                {/* 상품수량 */}
-                <td>{v.num}</td>
-                {/* 상품가격 총합계 */}
-                <td>{addComma(v.ginfo[3] * v.num)}원</td>
-                {/* 삭제버튼 */}
-                <td>
-                  <button className="cfn" data-idx={v.idx}>
-                    ×
-                  </button>
-                </td>
-              </tr>
-            ))}
+    // 리스트 숨기기 함수 //////////
+    const hideList = (e) => {
+        e.preventDefault();
+        $("#cartlist").animate({ right: "-60%" }, 600);
+    }; //////////// hideList ////////////
 
-            <tr>
-              <td colSpan="6">총합계 :</td>
-              <td>999,000원</td>
-              <td></td>
-            </tr>
-          </tbody>
-        </table>
-      </section>
-      {/* 카트버튼이미지 박스 */}
-      <div id="mycart" onClick={showList}>
-        {/* 카트이미지 */}
-        <img src="./images/mycart.gif" title="개의 상품이 있습니다" />
-        {/* 카트상품개수 출력박스 */}
-        <div className="cntBx">{cntData}</div>
-      </div>
-    </>
-  );
+    // 리스트 삭제 함수 /////////////////
+    const deleteItem = (e) => {
+        const selIdx = $(e.target).attr("data-idx");
+        console.log("지울아이:", selIdx);
+
+        // 해당 데이터 순번 알아내기
+        const newData = selData.filter((v) => {
+            if (v.idx !== selIdx.idx) return true;
+        });
+
+        console.log('제거 후 리스트:',newData);
+
+    }; ///// deleteItem 함수 /////
+
+    /// 리턴 코드 ///////////////////////
+    return (
+        <>
+            <section id="cartlist">
+                <a href="#" className="cbtn cbtn2" onClick={hideList}>
+                    <span>닫기버튼</span>
+                </a>
+                <table>
+                    <caption>
+                        <h1> 카트 리스트</h1>
+                    </caption>
+                    <tbody>
+                        <tr>
+                            <th>상품</th>
+                            <th>번호</th>
+                            <th>상품명</th>
+                            <th>상품코드</th>
+                            <th>단가</th>
+                            <th>수량</th>
+                            <th>합계</th>
+                            <th>삭제</th>
+                        </tr>
+
+                        {selData.map((v, i) => (
+                            <tr key={i}>
+                                {/* 상품이미지 */}
+                                <td>
+                                    <img src={"images/goods/" + v.cat + "/" + v.ginfo[0] + ".png"} alt="item" />
+                                </td>
+                                {/* 리스트순번 */}
+                                <td>{i + 1}</td>
+                                {/* 상품명 */}
+                                <td>{v.ginfo[1]}</td>
+                                {/* 상품코드 */}
+                                <td>{v.ginfo[2]}</td>
+                                {/* 상품가격 */}
+                                <td>{addComma(v.ginfo[3])}원</td>
+                                {/* 상품수량 */}
+                                <td>{v.num}</td>
+                                {/* 상품가격 총합계 */}
+                                <td>{addComma(v.ginfo[3] * v.num)}원</td>
+                                {/* 삭제버튼 */}
+                                <td>
+                                    <button className="cfn" data-idx={v.idx} onClick={deleteItem}>
+                                        ×
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+
+                        <tr>
+                            <td colSpan="6">총합계 :</td>
+                            <td>{addComma(totalCnt)}원</td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </section>
+            {/* 카트버튼이미지 박스 */}
+            <div id="mycart" onClick={showList}>
+                {/* 카트이미지 */}
+                <img src="./images/mycart.gif" title="개의 상품이 있습니다" />
+                {/* 카트상품개수 출력박스 */}
+                <div className="cntBx">{cntData}</div>
+            </div>
+        </>
+    );
 }); ////////////// CartList 컴포넌트 /////////

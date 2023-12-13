d5fbb001cb55c20290000c33849c4c4cf3b76048
Choisj01 <choisujin96@naver.com>
2023-12-13 12:24:39 +0900

    Update index.js

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/index.js
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/index.js
@@ -21,6 +21,7 @@ import { Series } from "./components/pages/Series";
 import { SchPage } from "./components/pages/SchPage";
 import { Member } from "./components/pages/Member";
 import { Login } from "./components/pages/Login";
+import { Board } from "./components/pages/Board";
 
 /********************************************* 
     [ 리액트 라우터 ]
@@ -78,7 +79,7 @@ export default function App() {
               <Route path="games" element={<Games />} />
               <Route path="news" element={<News />} />
               <Route path="video" element={<Video />} />
-              <Route path="board" element={<SwiperApp />} />
+              <Route path="board" element={<Board />} />
               <Route path="detail" element={<CatDetail />} />
               <Route path="schpage" element={<SchPage />} />
               <Route path="member" element={<Member />} />

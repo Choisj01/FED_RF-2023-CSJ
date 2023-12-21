b14d478a8818733f6f7ccd52102a9e2636ecf737
Choisj01 <choisujin96@naver.com>
2023-12-21 12:20:32 +0900

    Create GList.jsx

---
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/GList.jsx
@@ -0,0 +1,64 @@
+// 상품전체 리스트 페이지
+
+export function GList(){
+
+    return(
+        <>
+            <main id="cont"><section><div id="optbx"><label for="men">남성</label> <input type="checkbox" id="men"> <label for="women">여성</label> <input type="checkbox" id="women"> <label for="style">스타일</label> <input type="checkbox" id="style"></div> <div classname="grid"><div><a href="#/detail" classname="">
+                [1]
+                    <img src="./images/goods/men/m1.png" alt="dress"> <aside><h2>[남성]카모전판프린트 PQ 티셔츠</h2> <h3>99,000원</h3></aside></a></div><div><a href="#/detail" classname="">
+                [2]
+                    <img src="./images/goods/style/m7.png" alt="dress"> <aside><h2>[스타일]블럭형 풀집업 래쉬가드</h2> <h3>99,000원</h3></aside></a></div><div><a href="#/detail" classname="">
+                [3]
+                    <img src="./images/goods/men/m8.png" alt="dress"> <aside><h2>[남성]베이직 솔리드 래쉬가드</h2> <h3>49,000원</h3></aside></a></div><div><a href="#/detail" classname="">
+                [4]
+                    <img src="./images/goods/style/m8.png" alt="dress"> <aside><h2>[스타일]베이직 솔리드 래쉬가드</h2> <h3>49,000원</h3></aside></a></div><div><a href="#/detail" classname="">
+                [5]
+                    <img src="./images/goods/women/m2.png" alt="dress"> <aside><h2>[여성]빅로고 컬러 블럭 PQ 티셔츠</h2> <h3>89,000원</h3></aside></a></div><div><a href="#/detail" classname="">
+                [6]
+                    <img src="./images/goods/men/m9.png" alt="dress"> <aside><h2>[남성]남성 루즈핏 슬리브리스</h2> <h3>59,000원</h3></aside></a></div><div><a href="#/detail" classname="">
+                [7]
+                    <img src="./images/goods/women/m3.png" alt="dress"> <aside><h2>[여성]빅로고 컬러 블럭 PQ 티셔츠</h2> <h3>89,000원</h3></aside></a></div><div><a href="#/detail" classname="">
+                [8]
+                    <img src="./images/goods/style/m1.png" alt="dress"> <aside><h2>[스타일]카모전판프린트 PQ 티셔츠</h2> <h3>99,000원</h3></aside></a></div><div><a href="#/detail" classname="">
+                [9]
+                    <img src="./images/goods/women/m4.png" alt="dress"> <aside><h2>[여성]부분 스트라이프 PQ 티셔츠</h2> <h3>99,000원</h3></aside></a></div><div><a href="#/detail" classname="">
+                [10]
+                    <img src="./images/goods/style/m2.png" alt="dress"> <aside><h2>[스타일]빅로고 컬러 블럭 PQ 티셔츠</h2> <h3>89,000원</h3></aside></a></div><div><a href="#/detail" classname="">
+                [11]
+                    <img src="./images/goods/style/m3.png" alt="dress"> <aside><h2>[스타일]빅로고 컬러 블럭 PQ 티셔츠</h2> <h3>89,000원</h3></aside></a></div><div><a href="#/detail" classname="">
+                [12]
+                    <img src="./images/goods/women/m1.png" alt="dress"> <aside><h2>[여성]카모전판프린트 PQ 티셔츠</h2> <h3>99,000원</h3></aside></a></div><div><a href="#/detail" classname="">
+                [13]
+                    <img src="./images/goods/women/m5.png" alt="dress"> <aside><h2>[여성]웰딩포인트 트레이닝 하프팬츠</h2> <h3>89,000원</h3></aside></a></div><div><a href="#/detail" classname="">
+                [14]
+                    <img src="./images/goods/men/m2.png" alt="dress"> <aside><h2>[남성]빅로고 컬러 블럭 PQ 티셔츠</h2> <h3>89,000원</h3></aside></a></div><div><a href="#/detail" classname="">
+                [15]
+                    <img src="./images/goods/men/m3.png" alt="dress"> <aside><h2>[남성]빅로고 컬러 블럭 PQ 티셔츠</h2> <h3>89,000원</h3></aside></a></div><div><a href="#/detail" classname="">
+                [16]
+                    <img src="./images/goods/women/m6.png" alt="dress"> <aside><h2>[여성]블럭형 풀집업 래쉬가드</h2> <h3>99,000원</h3></aside></a></div><div><a href="#/detail" classname="">
+                [17]
+                    <img src="./images/goods/women/m9.png" alt="dress"> <aside><h2>[여성]여성 루즈핏 슬리브리스</h2> <h3>59,000원</h3></aside></a></div><div><a href="#/detail" classname="">
+                [18]
+                    <img src="./images/goods/men/m4.png" alt="dress"> <aside><h2>[남성]부분 스트라이프 PQ 티셔츠</h2> <h3>99,000원</h3></aside></a></div><div><a href="#/detail" classname="">
+                [19]
+                    <img src="./images/goods/women/m7.png" alt="dress"> <aside><h2>[여성]블럭형 풀집업 래쉬가드</h2> <h3>99,000원</h3></aside></a></div><div><a href="#/detail" classname="">
+                [20]
+                    <img src="./images/goods/women/m8.png" alt="dress"> <aside><h2>[여성]베이직 솔리드 래쉬가드</h2> <h3>49,000원</h3></aside></a></div><div><a href="#/detail" classname="">
+                [21]
+                    <img src="./images/goods/men/m7.png" alt="dress"> <aside><h2>[남성]블럭형 풀집업 래쉬가드</h2> <h3>99,000원</h3></aside></a></div><div><a href="#/detail" classname="">
+                [22]
+                    <img src="./images/goods/style/m4.png" alt="dress"> <aside><h2>[스타일]부분 스트라이프 PQ 티셔츠</h2> <h3>99,000원</h3></aside></a></div><div><a href="#/detail" classname="">
+                [23]
+                    <img src="./images/goods/men/m5.png" alt="dress"> <aside><h2>[남성]웰딩포인트 트레이닝 하프팬츠</h2> <h3>89,000원</h3></aside></a></div><div><a href="#/detail" classname="">
+                [24]
+                    <img src="./images/goods/style/m5.png" alt="dress"> <aside><h2>[스타일]웰딩포인트 트레이닝 하프팬츠</h2> <h3>89,000원</h3></aside></a></div><div><a href="#/detail" classname="">
+                [25]
+                    <img src="./images/goods/men/m6.png" alt="dress"> <aside><h2>[남성]블럭형 풀집업 래쉬가드</h2> <h3>99,000원</h3></aside></a></div><div><a href="#/detail" classname="">
+                [26]
+                    <img src="./images/goods/style/m6.png" alt="dress"> <aside><h2>[스타일]블럭형 풀집업 래쉬가드</h2> <h3>99,000원</h3></aside></a></div><div><a href="#/detail" classname="">
+                [27]
+                    <img src="./images/goods/style/m9.png" alt="dress"> <aside><h2>[스타일]루즈핏 슬리브리스</h2> <h3>59,000원</h3></aside></a></div></div></section></main>
+        </>
+    );
+} ////////// GList 컴포넌트 ///////////////////

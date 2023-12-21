b1e95d17a1766fb22d6f083fe3d296c244a790c0
Choisj01 <choisujin96@naver.com>
2023-12-21 12:20:19 +0900

    Update gnb.js

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/data/gnb.js
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/data/gnb.js
@@ -1,23 +1,9 @@
 // Pilot PJ GNB 페이지별 메뉴데이터 JS - gnb.js
 
 export const gnbData = {
-    "main":["MEN","WOMEN","STYLE"],
-    "men":[
-        "NEW MEN'S ARRIVAL",
-        "WINDBREAKER",
-        "BEACH STYLE",
-        "SPORT STYLE",
-    ],
-    "women":[
-        "NEW WOMEN'S ARRIVAL",
-        "SPORTY FASHION",
-        "FREE STYLE",
-        "COMFORTABLE STYLE",
-    ],
-    "style":[
-        "SPECIAL SUMMER STYLE",
-        "GOLF LIFE",
-        "CAMPING STYLE",
-        "SPORT STYLE",
-    ],
+    main: ["MEN", "WOMEN", "STYLE"],
+    men: ["NEW MEN'S ARRIVAL", "WINDBREAKER", "BEACH STYLE", "SPORT STYLE"],
+    women: ["NEW WOMEN'S ARRIVAL", "SPORTY FASHION", "FREE STYLE", "COMFORTABLE STYLE"],
+    style: ["SPECIAL SUMMER STYLE", "GOLF LIFE", "CAMPING STYLE", "SPORT STYLE"],
+    glist: ["Filter List", "Paging List", "More List"],
 }; /////////gnbData////////////

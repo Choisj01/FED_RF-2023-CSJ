d6303d30ce115043322d92fdc057893063938397
Choisj01 <choisujin96@naver.com>
2023-12-21 15:19:48 +0900

    Update GList.jsx

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/GList.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/GList.jsx
@@ -1,30 +1,42 @@
 // 상품전체 리스트 페이지
 
 // 상품전체 리스트 CSS 불러오기
+import { useEffect, useState } from "react";
 import "../css/glist.css";
 
 // 상품데이터 불러오기
 import gdata from "../data/glist-items";
+import { ItemDetail } from "../modules/ItemDetail";
 
-console.log("전체Data:",gdata);
+console.log("전체Data:", gdata);
 
 export function GList() {
-    const makeList = () => {
-        return gdata.map((v, i) => (
+    // 상태관리 변수
+    // 1. 아이템 코드(m1,m2,m3, ...)
+    const [item, setItem] = useState("m1");
+    // 2. 카테고리명(men/women/style)
+    const [catName, setCatName] = useState("men");
+    // 3. 상세보기 모드(true/fales)
+    const [detailSts, setDetailSts] = useState(true);
+
+    // 리스트 만들기 함수
+    const makeList = () =>
+        gdata.map((v, i) => (
             <div key={i}>
                 <a href="#">
-                    [{i+1}]
-                    <img 
-                    src={"./images/goods/" + v.cat + "/" + v.ginfo[0] + ".png"} 
-                    alt="dress" />
+                    [{i + 1}]
+                    <img src={"./images/goods/" + v.cat + "/" + v.ginfo[0] + ".png"} alt="dress" />
                     <aside>
                         <h2>{v.ginfo[1]}</h2>
                         <h3>{addComma(v.ginfo[3])}원</h3>
                     </aside>
                 </a>
             </div>
-        ));
-    }; ///////////////// makeList함수 ////////////////////
+        )); ////////////// makeList함수 ///////////
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
 
     //정규식함수(숫자 세자리마다 콤마해주는 기능)
     function addComma(x) {
@@ -40,12 +52,30 @@ export function GList() {
                     <label htmlFor="men">남성</label>
                     <input type="checkbox" id="men" defaultChecked />
                     <label htmlFor="women">여성</label>
-                    <input type="checkbox" id="women" defaultChecked  />
+                    <input type="checkbox" id="women" defaultChecked />
                     <label htmlFor="style">스타일</label>
                     <input type="checkbox" id="style" defaultChecked />
                 </div>
                 <div className="grid">{makeList()}</div>
             </section>
+            {
+                /* 2.5. 상세보기 박스 */
+                detailSts && (
+                    <div
+                        className="bgbx"
+                        style={{
+                            position: " fixed",
+                            top: 0,
+                            paddingTop: "12vh",
+                            backdropFilter: "blur(8px)",
+                            height: "88vh",
+                            zIndex: 9999,
+                        }}
+                    >
+                        <ItemDetail goods={item} cat={catName} />
+                    </div>
+                )
+            }
         </main>
     );
 } ///////////////////////////////// GList 컴포넌트 /////////////////////////////

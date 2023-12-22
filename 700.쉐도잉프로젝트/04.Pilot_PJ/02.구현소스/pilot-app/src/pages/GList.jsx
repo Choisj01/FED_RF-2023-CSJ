10fa30fd13050456a04f84269b2e5b759d051892
Choisj01 <choisujin96@naver.com>
2023-12-22 17:24:35 +0900

    Update GList.jsx

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/GList.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/GList.jsx
@@ -22,10 +22,12 @@ export function GList() {
 
     // 리랜더링을 위한 상태변수 : 무조건 리랜더링 설정 목적
     const [force, setForce] = useState(null);
+    // 데이터 상태 관리 변수
+    const [currData,setCurrData] = useState(gdata);
 
     // 리스트 만들기 함수 //////////////////////////////
     const makeList = () =>
-        gdata.map((v, i) => (
+        currData.map((v, i) => (
             <div key={i}>
                 <a
                     href="#"
@@ -71,6 +73,88 @@ export function GList() {
         $(".bgbx").slideDown(600);
     }; ///////////showDetail 함수 //////////////
 
+    /*****************************************************
+       함수명 : changeList
+       기능 :  체크박스에 따른 리스트 변경하기
+    *****************************************************/
+    const changeList = (e) => {
+          // 1. 체크박스 아이디
+          const cid = e.target.id;
+
+          //2. 체크박스 체크여부 : checked (true/false)
+          const chked = e.target.checked;
+          console.log("아이디는:", cid, chked);
+
+           // 3. 체크박스 체크개수세기 : 1개 초과시 배열 합치기
+        let num = $(".chkbx:checked").length;
+        console.log("체크개수:", num);
+
+           // 4. 기존 입력데이터 가져오기
+        // 현재 상태관리 데이터 배열값 ( 변경되는 데이터 )
+        let temp = currData;
+
+        // 결과집합 배열변수 : 최종결과배열
+        let lastList = [];
+
+        // 5. 체크박스 체크유무에 따른 분기
+        // (1) 체크박스가 true일때 해당 검색어로 검색하기
+        if (chked) {
+            // 현재 데이터 변수에 담기 (원본데이터로부터!)
+            const nowList = gdata.filter(v=>{
+                if(v.cat===cid) return true;
+            }) ////// filter ////////
+
+            // 체크개수가 1초과일때 배열합치기
+            if (num > 1) {
+                // 스프레드 연산자(...) 사용!
+                lastList = [...temp, ...nowList];
+            } /////// if ////////
+            else {
+                // 체크박스 하나일때
+                lastList = nowList;
+            } ///// else ///////
+
+            console.log('추가구역:',lastList);
+        } ////////// if ///////////
+
+        // (2) 체크박스가 false일때 데이터 지우기!
+        else {
+            console.log("지울데이터:", cid);
+            // for문을 돌면서 배열데이터 중 해당값을 지운다!
+            for (let i = 0; i < temp.length; i++) {
+                // -> 삭제대상:
+                // 데이터 중 cat 항목값이 아이디명과 같은것
+                if (temp[i].cat == cid) {
+                    // 해당 항목 지우기
+                    // 배열지우기 메서드 : splice(순번,개수)
+                    temp.splice(i, 1);
+                    // 주의! 배열을 지우면 전체개수가 1씩 줄어든다!
+                    // 반드시 줄임처리할 것!
+                    i--;
+
+                    // 참고테스트 :배열삭젝 delete는 무엇인가?
+                    // delete 배열지우기는 값만 지우고 주소는 남는다!(자리는 있고 값만 지움!)
+                    // 지운 후 값은 undefined로 남아진다!
+                    // delete temp[i];
+                    // -> 리스트 처리시 에러발생함!
+                    //여기서는 splice를 반드시 사용할 것!
+                } /////////// if////////
+            } //////////// for ///////////
+
+            console.log("삭제처리된 배열:", temp);
+
+            // 결과처리하기 : 삭제처리된 temp를 결과에 넣기!
+            lastList = temp;
+
+            // 6. 검색결과 리스트 업데이트 하기
+            // setCurrData(temp);
+            console.log('삭제구역:',lastList);
+        } //////// else ///////////
+
+      
+      
+    }; //////////// changeList 함수 ///////////
+
     // 리턴 코드 ///////////////////////////////////////
     return (
         <main id="cont">
@@ -78,11 +162,14 @@ export function GList() {
             <section>
                 <div id="optbx">
                     <label htmlFor="men">남성</label>
-                    <input type="checkbox" id="men" defaultChecked />
+                    <input type="checkbox" className="chkbx" id="men" defaultChecked  
+                    onChange={changeList}/>
                     <label htmlFor="women">여성</label>
-                    <input type="checkbox" id="women" defaultChecked />
+                    <input type="checkbox" className="chkbx" id="women" defaultChecked 
+                    onChange={changeList}/>
                     <label htmlFor="style">스타일</label>
-                    <input type="checkbox" id="style" defaultChecked />
+                    <input type="checkbox" className="chkbx" id="style" defaultChecked 
+                    onChange={changeList}/>
                 </div>
                 <div className="grid">{makeList()}</div>
             </section>

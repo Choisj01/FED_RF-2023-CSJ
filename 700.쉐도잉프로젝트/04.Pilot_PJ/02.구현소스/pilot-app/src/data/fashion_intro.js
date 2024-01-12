019a067fe5c532dc4f44fe6acc1e0febd90a8f0d
Choisj01 <choisujin96@naver.com>
2024-01-12 12:01:09 +0900

    dd

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/data/fashion_intro.js
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/data/fashion_intro.js
@@ -20,20 +20,28 @@ export const fsData = {
       ["WOMEN'S", "STYLE"],
     ],
   },
+
+  // 서브 정보 구성하기 /////
+  submen1: {
+    isrc: "./images/sub/men/03.disc.png",
+    ialt: "공유가 해변에 있다!",
+    tit: ["MEN'S", "BEACH STYLE"],
+  },
+
   // 서브페이지용 데이터는 구성이 다름
   sub: {
     men: [
       {
-        isrc: "./images/men02.png",
-        ialt: "공유다!",
-        tit: ["MEN'S", "ESSENTIALS"],
+        isrc: "./images/sub/men/03.disc.png",
+        ialt: "공유가 해변에 있다!",
+        tit: ["MEN'S", "BEACH STYLE"],
       },
       {
-        isrc: ["./images/style02.png", "./images/style03.png"],
-        ialt: ["여자스타일", "공유스타일"],
+        isrc: ["./images/sub/men/04.disc.png", "../images/sub/men/05.disc.png"],
+        ialt: ["남자스타일1", "남자스타일2"],
         tit: [
-          ["MEN'S", "STYLE"],
-          ["WOMEN'S", "STYLE"],
+          ["MEN'S", "SPORT STYLE"],
+          ["MEN'S", "LIFE STYLE"],
         ],
       },
     ],

9fdcd753c118b86d586f29a5708462394008c688
Choisj01 <choisujin96@naver.com>
2024-01-16 17:03:34 +0900

    Update Banner.jsx

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/Banner.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/Banner.jsx
@@ -11,29 +11,35 @@ export function Banner() {
     // 리스트를 만드는 함수
     const makeList = (gubun) => {
         // gubun : 1 - 배너 / 0 - 블릿
+
+        // 참고)
+        // 0/1로 false/true를 대신하면 리액트에서는
+        // 이 숫자를 하단에 출력함! 따라서 true/false로 
+        // 변경하여 코딩하면 이 문제는 해결됨!
+
         // for문을 돌려서 태그를 생성할때 배열에 담는다!
         // -> 문자형이 아닌 JSX의 태그이므로 배열에 담고 
         // for문 없이 그대로 태그를 출력할 수 있다!!
         let hcode = [];
 
-        for (let i = 0; i < BAN_CNT; i++) {
-            if (gubun) {
-                // 배너코드
+        for(let i=0;i<BAN_CNT;i++){
+            if(gubun){ // 배너코드
                 hcode[i] =
-                    <li className={"ban" + (i == 0 ? "6" : i)} key={i}>
-                        <span className="ir">배너{i == 0 ? "6" : i}</span>
-                    </li>
-            } 
-            else {
-                // 블릿코드
-                hcode[i] = 
-                    <li className={i == 0 ? "on" : ""} key={i}>
-                        <a href="#">
-                            <span className="bld">블릿</span>
-                        </a>
-                    </li>
+                <li className={'ban'+(i==0?'6':i)} key={i}>
+                    <span className="ir">배너{i==0?'6':i}</span>
+                </li>
             }
-        } //////////////////// for ///////////////
+            else{ // 블릿코드
+                hcode[i] =
+                <li className={i==0?'on':''} key={i}>
+                    <a href="#">
+                        <span className="bld">블릿</span>
+                    </a>
+                </li>
+            }
+
+        } //////// for //////////
+        
         // console.log(hcode);
 
         // 코드리턴

0390ab916b78b158dbe10207a8c6e95796dec8da
Choisj01 <choisujin96@naver.com>
2024-01-17 12:49:48 +0900

    ㅇㅇ

--- 005.SVG학습/js/movie_intro.js
+++ 005.SVG학습/js/movie_intro.js
@@ -27,3 +27,38 @@ setTimeout(()=>{
 setTimeout(()=>{
     logoAni.addClass('anion');
 },12000);
+
+
+/* 오디오 컨트롤 하기 */
+// 대상 : .play-box
+$('.play-box').hover(
+    function(){ // over
+    console.log('오버야!');
+    // 오버시 진한 이미지로 변경
+
+    // 기존값 읽어오기
+    let csrc = $('img',this).attr('src');
+    // 경로에서 '.png'를 '-1.png' 로 변경
+    $('img',this).attr('src',csrc.replace('.png','-1.png'));   
+},
+function(){ //out
+    console.log('아웃이야');
+
+    // 기존값 읽어오기
+    let csrc = $('img',this).attr('src');
+    // 경로에서 '.png'를 '-1.png' 로 변경
+    $('img',this).attr('src',csrc.replace('-1.png','.png'));   
+
+}) ///////// hover //////////////
+// 클릭시 이미지 변경하기 + 오디오 재생/멈춤하기
+.on('click',function(){
+    console.log('클릭이야~!!');
+
+    // 오디오 요소 : 제이쿼리는  get(0)으로 요소를 선택
+    const myAudio = $('.my-audio');
+    
+    // 현재 오디오 멈춤여부 알아오기
+    const isPaused = 
+
+
+});
